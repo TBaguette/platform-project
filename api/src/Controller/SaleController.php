0dc55eae55b97bc97e86380ba7b06b1a9c0bfeff
Quentin Vauthier <quentin.vauthier000@gmail.com>
2022-12-29 23:35:40 +0100

    Requêtepour le compte par jour, mois ou année

--- api/src/Controller/SaleController.php
+++ api/src/Controller/SaleController.php
@@ -12,19 +12,44 @@ use Doctrine\Persistence\ManagerRegistry;
 #[Route("/sales")]
 class SaleController extends AbstractController 
 {
-    #[Route('/count-by-year', name: 'count_by_year', methods: ['GET'])]
-    public function countByYear(ManagerRegistry $doctrine): JsonResponse
-    {
+    #[Route('/countby/{type}', name: 'count', methods: ['GET'])]
+    public function countBy(string $type, ManagerRegistry $doctrine): JsonResponse {
         $em = $doctrine->getManager();
 
+        $query = null;
         $query = $em->createQuery(
-            'SELECT s1.zip as zip,
-                    ((COUNT(s1) * 100) / SUM(s1)) as count
-            FROM App\Entity\Sale s1
-            GROUP BY s1.zip'
+            'SELECT sale.date as date
+            FROM App\Entity\Sale sale'
         );
         $results = $query->getResult();
+        $final = [];
+        for($i = 0; $i < count($results); $i++) {
+            switch($type) {
+                case 'day':
+                    if(isset($final[$results[$i]['date']->format('d/m/Y')])) {
+                        $final[$results[$i]['date']->format('d/m/Y')]++;
+                    } else {
+                        $final[$results[$i]['date']->format('d/m/Y')] = 1;
+                    }
+                    break;
+                case 'month':
+                    if(isset($final[$results[$i]['date']->format('m/Y')])) {
+                        $final[$results[$i]['date']->format('m/Y')]++;
+                    } else {
+                        $final[$results[$i]['date']->format('m/Y')] = 1;
+                    }
+                    break;
+                case 'year':
+                default:
+                    if(isset($final[$results[$i]['date']->format('Y')])) {
+                        $final[$results[$i]['date']->format('Y')]++;
+                    } else {
+                        $final[$results[$i]['date']->format('Y')] = 1;
+                    }
+                    break;
+            }
+        }
 
-        return new JsonResponse(json_encode($results), 200, [], true);
+        return new JsonResponse(json_encode($final), 200, [], true);
     }
 }
