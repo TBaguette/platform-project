9f24583d36750c4d450d5c59f9a7741f7a3f90b9
Lewynn <jire1812@gmail.com>
2022-12-30 22:44:14 +0100

    changement années en mois

--- pwa/components/diagram/LinearDiagram.tsx
+++ pwa/components/diagram/LinearDiagram.tsx
@@ -1,135 +1,125 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {BaseType} from "d3";
 import * as d3 from "d3";
 import InputLinearComponent from "../input/InputLinearDiagram";
 
 const LinearComponent = () => {
+    const refChart = useRef(null);
+    const [data, setData] = useState([]);
+    const [type, setType] = useState('appartement');
 
-  const refChart = useRef(null);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/sales/price-evolution/' + type);
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    }, [refChart, type]);
 
-  useEffect(() => {
-    const data = [
-      { year: 2017, price: 100 },
-      { year: 2018, price: 110 },
-      { year: 2019, price: 105 },
-      { year: 2020, price: 115 },
-      { year: 2021, price: 120 }
-    ];
+    useEffect(() => {
+        const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+        LinearChartSVG(refChart.current, data, margin, {
+            width: 500 - margin.left - margin.right,
+            height: 500 - margin.top - margin.bottom
+        });
+    }, [data]);
 
-    LinearChartSVG(refChart.current, data, margin, {
-      width: 500 - margin.left - margin.right,
-      height: 500 - margin.top - margin.bottom
-    });
-  }, [refChart]);
-
-  return (<div className="chart" ref={refChart}>
-    <InputLinearComponent/>
-  </div>)
+    return (<div className="chart" ref={refChart}>
+        <InputLinearComponent setType={setType}/>
+    </div>)
 }
 
-const LinearChartSVG = (element: BaseType, data: { year: number, price: number }[], margin: {top: number, right: number, bottom: number, left: number}, options : {
+const LinearChartSVG = (element: BaseType, data: { label: string, price: number }[], margin: {top: number, right: number, bottom: number, left: number}, options : {
   width: number,
   height: number
 }) => {
+    if(data.length < 2) return;
 
-  d3
-    .select(element)
-    .select('svg')
-    .remove();
+    d3.select(element)
+        .select('svg')
+        .remove();
 
-  const svg = d3.select(element)
-    .append('svg')
-    .attr('width', options.width + margin.left + margin.right)
-    .attr('height', options.height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+    const svg = d3.select(element)
+        .append('svg')
+        .attr('width', options.width + margin.left + margin.right)
+        .attr('height', options.height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  // ajouter un titre au diagramme
-  svg.append("text")
-    .attr("x", options.width / 2) // positionner le titre au milieu de l'axe des X
-    .attr("y", margin.top) // positionner le titre en haut du diagramme
-    .attr("text-anchor", "middle") // centrer le titre
-    .attr("font-size", "20px") // définir la taille du titre
-    .attr("font-family", "Roboto Slab")
-    .text("Prix moyen du mètre carré");
+    svg.append("text")
+        .attr("x", options.width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .attr("font-family", "Roboto Slab")
+        .text("Prix moyen du mètre carré");
 
-  // définir l'échelle pour l'axe des X
-  let x = d3.scaleTime()
-    .domain([new Date(2017, 0, 1), new Date(2021, 0, 1)]) // définir l'intervalle de données pour l'axe des X
-    .range([0, options.width]); // définir l'intervalle de l'espace de l'axe des X
+    let x = d3.scaleTime()
+        .domain([
+            new Date(data[0].label.split("/")[1], parseInt(data[0].label.split("/")[0])-1),
+            new Date(data[data.length-1].label.split("/")[1], parseInt(data[data.length-1].label.split("/")[0])-1)
+        ])
+        .range([0, options.width]);
 
-// définir l'échelle pour l'axe des Y
-  let y = d3.scaleLinear()
-    .domain([0, 200]) // définir l'intervalle de données pour l'axe des Y
-    .range([options.height, 0]); // définir l'intervalle de l'espace de l'axe des Y
+    let y = d3.scaleLinear()
+        .domain([Math.min(...data.map(item => item.value))-10, Math.max(...data.map(item => item.value))+10])
+        .range([options.height, 0]);
 
-  // ajouter un groupe pour l'axe des X
-  let xAxisGroup = svg.append("g")
-    .attr("transform", "translate(0," + (options.height) + ")");
+    let xAxisGroup = svg.append("g")
+        .attr("transform", "translate(0," + (options.height) + ")");
 
-  // ajouter un groupe pour l'axe des Y
-  let yAxisGroup = svg.append("g")
-    .attr("transform", "translate(0,0)");
+    let yAxisGroup = svg.append("g")
+        .attr("transform", "translate(0,0)");
 
-  // ajouter une étiquette "Année" à l'axe des X
-  xAxisGroup.append("text")
-    .attr("x", options.width / 2) // positionner l'étiquette au milieu de l'axe des X
-    .attr("y", margin.bottom - 5) // décaler l'étiquette vers le bas de l'axe
-    .attr("text-anchor", "middle") // centrer l'étiquette sur l'axe
-    .text("Année");
+    xAxisGroup.append("text")
+        .attr("x", options.width / 2)
+        .attr("y", margin.bottom - 5)
+        .attr("text-anchor", "middle")
+        .text("Année");
+    
+    yAxisGroup.append("text")
+        .attr("x", -options.height / 2)
+        .attr("y", -margin.left + 10)
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("Prix");
 
-  // ajouter une étiquette "Prix" à l'axe des Y
-  yAxisGroup.append("text")
-    .attr("x", -options.height / 2) // positionner l'étiquette au milieu de l'axe des Y
-    .attr("y", -margin.left + 10) // décaler l'étiquette vers la gauche de l'axe
-    .attr("text-anchor", "middle") // aligner l'étiquette à gauche de l'axe
-    .attr("transform", "rotate(-90)") // faire pivoter l'étiquette de 90 degrés
-    .text("Prix");
+    let xAxis = d3.axisBottom(x)
+        .ticks(5)
+        .tickFormat(d3.timeFormat("%m/%Y"))
+        .tickSize(0);
 
-  // créer l'axe des X en utilisant l'échelle pour l'axe des X
-  let xAxis = d3.axisBottom(x)
-    .ticks(5) // définir le nombre de marques sur l'axe
-    .tickFormat(d3.timeFormat("%Y")) // définir le format de l'axe
-    .tickSize(0);
+    let yAxis = d3.axisLeft(y)
+        .ticks(5)
+        .tickSize(0);
 
-  // créer l'axe des Y en utilisant l'échelle pour l'axe des Y
-  let yAxis = d3.axisLeft(y)
-    .ticks(5) // définir le nombre de marques sur l'axe
-    .tickSize(0);
+    xAxisGroup.call(xAxis);
 
-  // ajouter l'axe des X au groupe de l'axe des X
-  xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 
-  // ajouter l'axe des Y au groupe de l'axe des Y
-  yAxisGroup.call(yAxis);
+    let points = svg.selectAll(".point")
+        .data(data)
+        .enter(); 
 
-  // sélectionner et lier les données
-  let points = svg.selectAll(".point")
-    .data(data)
-    .enter(); // créer des éléments pour chaque point de données
+    points.append("circle")
+        .attr("cx", (d) => x(new Date(d.label.split('/')[1], parseInt(d.label.split('/')[0])-1)))
+        .attr("cy", (d) => y(d.value))
+        .attr("r", 5)
+        .attr("fill", "steelblue")
+        .attr("class", "point");
 
-  // ajouter des points à l'aide de balises <circle>
-  points.append("circle")
-    .attr("cx", (d) => x(d.year)) // définir la position en X des points en utilisant l'échelle pour l'axe des X
-    .attr("cy", (d) => y(d.price)) // définir la position en Y des points en utilisant l'échelle pour l'axe des Y
-    .attr("r", 5) // définir le rayon des points
-    .attr("fill", "steelblue") // définir la couleur des points
-    .attr("class", "point"); // ajouter une classe aux points
+    let line = d3.line()
+        .x(function(d) { return x(new Date(d.label.split('/')[1], parseInt(d.label.split('/')[0])-1)); })
+        .y(function(d) { return y(d.value); })
 
-  // définir la méthode pour tracer une ligne
-  let line = d3.line()
-    .x(function(d) { return x(new Date(d.year, 0, 1)); }) // utiliser l'échelle pour l'axe des X pour mapper les données d'année sur l'espace de l'axe des X
-    .y(function(d) { return y(d.price); })
-
-  // ajouter une balise <path> à l'SVG pour tracer la ligne
-  svg.append("path")
-    .datum(data) // définir les données pour la ligne
-    .attr("fill", "none") // enlever le remplissage
-    .attr("stroke", "steelblue") // définir la couleur de la ligne
-    .attr("stroke-width", 1.5) // définir l'épaisseur de la ligne
-    .attr("d", line); // utiliser la fonction de tracé de ligne pour définir le d attribut
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 };
 
 export default LinearComponent;
--- pwa/components/input/InputLinearDiagram.tsx
+++ pwa/components/input/InputLinearDiagram.tsx
@@ -1,10 +1,18 @@
-const InputLinearComponent = () => {
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  setType: Dispatch<SetStateAction<string>>;
+}
+
+const InputLinearComponent = ({ setType } : Props) => {
 
   return (
     <form>
       <label>
         Sélectionnez un type de logement :
-        <select>
+        <select
+          onChange={(e) => { setType(e.target.value) }}
+          >
           <option value="appartement">Appartement</option>
           <option value="maison">Maison</option>
         </select>
