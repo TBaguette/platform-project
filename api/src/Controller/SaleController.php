02968b39eb5f44ebd500aa409b400a784af2bd3d
Nathan RIETTE <nathan.riette@etu.univ-lehavre.fr>
2022-12-30 16:03:58 +0100

    push fixture bundle load with departement name instand of code + finish route count by year

--- api/src/Controller/SaleController.php
+++ api/src/Controller/SaleController.php
@@ -2,29 +2,79 @@
 
 namespace App\Controller;
 
-
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Sale;
 
 #[Route("/sales")]
 class SaleController extends AbstractController 
 {
-    #[Route('/count-by-year', name: 'count_by_year', methods: ['GET'])]
-    public function countByYear(ManagerRegistry $doctrine): JsonResponse
+    #[Route('/count-by-year/{year}', name: 'count_by_year', methods: ['GET'])]
+    public function countByDepAndYear(ManagerRegistry $doctrine, int $year): JsonResponse
     {
         $em = $doctrine->getManager();
 
-        $query = $em->createQuery(
-            'SELECT s1.zip as zip,
-                    ((COUNT(s1) * 100) / SUM(s1)) as count
-            FROM App\Entity\Sale s1
-            GROUP BY s1.zip'
-        );
-        $results = $query->getResult();
+        $startDate = new \DateTimeImmutable("{$year}-01-01");
+        $endDate = new \DateTimeImmutable("{$year}-12-31");
 
-        return new JsonResponse(json_encode($results), 200, [], true);
+        $queryBuilder = $em->createQueryBuilder()
+            ->select('s.zip as zip')
+            ->addSelect('COUNT(s) as count')
+            ->from(Sale::class, 's')
+            ->where('s.date BETWEEN :start_date AND :end_date')
+            ->setParameter('start_date', $startDate)
+            ->setParameter('end_date', $endDate)
+            ->groupBy('s.zip');
+
+        $results = $queryBuilder->getQuery()->getResult();
+
+        $total = 0;
+
+        foreach ($results as $key => $value) {
+            $total += $value['count'];
+        }
+
+        //transform zip inside label and count inside value all in json
+        $json = [];
+        array_push($json, [
+            'label' => "Autres",
+            'value' => 0
+        ]);
+
+        foreach ($results as $key => $value) {
+            if($value['count'] * 100 / $total < 1) {
+                $json[0]['value'] += $value['count'] * 100 / $total;
+            } else {
+                array_push($json, [
+                    'label' => $value['zip'],
+                    'value' => $value['count'] * 100 / $total
+                ]);
+            }
+        }
+        return new JsonResponse(json_encode($json), 200, [], true);
+    }
+
+    #[Route('/price-evolution/{type}/{year}', name: 'sale_price_evolution', methods: ['GET'])]
+    public function getPriceEvolution(ManagerRegistry $doctrine, string $type, int $year): JsonResponse
+    {
+        $em = $doctrine->getManager();
+        
+        $queryBuilder = $em->createQueryBuilder()
+            ->select('MONTH(s.date) as month')
+            ->addSelect('AVG(s.price / s.surface) as avg_price')
+            ->from(Sale::class, 's')
+            ->where('s.type = :type')
+            ->andWhere('YEAR(s.date) = :year')
+            ->setParameter('type', $type)
+            ->setParameter('year', $year)
+            ->groupBy('month')
+            ->orderBy('month');
+
+        $results = $queryBuilder->getQuery()->getResult();
+
+        return new JsonResponse($results, 200);
     }
 }
--- api/src/DataFixtures/SalesFixtures.php
+++ api/src/DataFixtures/SalesFixtures.php
@@ -5,6 +5,8 @@ use App\Entity\Sale;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Monolog\DateTimeImmutable;
+//import la fonction getDepName
+include "src/Utils/depEnum.php";
 
 class SalesFixtures extends Fixture
 {
@@ -34,7 +36,8 @@ class SalesFixtures extends Fixture
                         $sale = new Sale();
                         $sale->price = floatval($line[10]);
                         $sale->surface = floatval($line[38]);
-                        $sale->zip = $line[16];
+                        //swtich from code dep to name dep
+                        $sale->zip = getDepName($line[18]);
                         $sale->type = $line[36];
                         $strdate = $line[8];
                         $array_date = explode('/', $strdate);
@@ -65,6 +68,7 @@ class SalesFixtures extends Fixture
 
 // 8 -> Date
 // 9 -> Type de vente
+// 18 -> Code département
 // 10 -> Prix
 // 36 -> Type local
 // 38 -> Surface bati
